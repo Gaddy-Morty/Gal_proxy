0929835993e3a3bb29673579dbf667fa92964d51
Dean Ma <mrdingma@gmail.com>
2020-02-04 16:14:02 -0800

    all 4 components connected version1

--- server/index.js
+++ server/index.js
@@ -12,10 +12,48 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+// IMAGE GALLERY PROXY RESPONSE
 app.get('/gallery/:id', (req, res) => {
   const id = req.params.id;
 
   axios.get(`http://localhost:3000/gallery/${id}`)
+    .then(({ data }) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// REVIEW PROXY RESPONSE
+app.get('/api/:id', (req, res) => {
+  const id = req.params.id;
+
+  axios.get(`http://localhost:3003/api/${id}`)
+    .then(({ data }) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// CALENDAR PROXY RESPONSE
+app.get('/month', (req, res) => {
+  axios.get('http://localhost:3001/month', {
+    params: req.query
+  })
+    .then(({ data }) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// RECS PROXY RESPONSE
+app.get('/listings', (req, res) => {
+  axios.get('http://localhost:3002/listings')
     .then(({ data }) => {
       res.json(data);
     })
