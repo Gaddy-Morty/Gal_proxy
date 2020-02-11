8e28b0138384fe5885b707264597847d229fd760
Dean Ma <mrdingma@gmail.com>
2020-02-11 11:49:54 -0800

    final

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// IMAGE GALLERY PROXY RESPONSE
+// GALLERY PROXY RESPONSE
 app.get('/gallery/:id', (req, res) => {
   const id = req.params.id;
 
-  axios.get(`http://localhost:3000/gallery/${id}`)
+  axios.get(`http://13.57.225.193:3000/gallery/${id}`)
     .then(({ data }) => {
       res.json(data);
     })
@@ -25,13 +25,36 @@ app.get('/gallery/:id', (req, res) => {
     });
 });
 
+// get gallery bundle
+app.get('/gallery', (req, res) => {
+  axios.get(`http://13.57.225.193:3000/bundle.js`)
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
 // REVIEW PROXY RESPONSE
 app.get('/api/:id', (req, res) => {
   const id = req.params.id;
 
-  axios.get(`http://localhost:3003/api/${id}`)
+  axios.get(`http://54.219.134.231:3003/api/${id}`)
     .then(({ data }) => {
       res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// get reviews bundle
+app.get('/reviews', (req, res) => {
+  axios.get(`http://54.219.134.231:3003/bundle.js`)
+    .then(({ data }) => {
+      res.send(data);
     })
     .catch((error) => {
       console.log(error);
@@ -40,7 +63,7 @@ app.get('/api/:id', (req, res) => {
 
 // CALENDAR PROXY RESPONSE
 app.get('/month', (req, res) => {
-  axios.get('http://localhost:3001/month', {
+  axios.get('http://52.53.187.170:3001/month', {
     params: req.query
   })
     .then(({ data }) => {
@@ -51,11 +74,45 @@ app.get('/month', (req, res) => {
     });
 });
 
+// get calendar css
+app.get('/calendar/css', (req, res) => {
+  axios.get(`http://52.53.187.170:3001/style.css`)
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// get calendar bundle
+app.get('/calendar', (req, res) => {
+  axios.get(`http://52.53.187.170:3001/bundle.js`)
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
 // RECS PROXY RESPONSE
 app.get('/listings', (req, res) => {
-  axios.get('http://localhost:3002/listings')
+  axios.get('http://13.52.217.109:3002/listings')
     .then(({ data }) => {
       res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// recs bundle
+app.get('/recs', (req, res) => {
+  axios.get(`http://13.52.217.109:3002/bundle.js`)
+    .then(({ data }) => {
+      res.send(data);
     })
     .catch((error) => {
       console.log(error);
